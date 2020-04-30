0d1b624ffe8d6d77743c511e502c50721b60bf31
Mayur <mayur.jaganmohan@gmail.com>
2020-04-30 21:37:25 +0530

    Updates

---
+++ precache-manifest.bd073ff168b1c145ae0a2cbbd684f902.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66061bd437cf8259fdd11e8ab8b1345e",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "8b677dd3274d7a6fdd17",
+    "url": "/covid-19-tracker/static/css/main.5cf0ec70.chunk.css"
+  },
+  {
+    "revision": "960e79211473020a059f",
+    "url": "/covid-19-tracker/static/js/2.0d491c23.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.0d491c23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b677dd3274d7a6fdd17",
+    "url": "/covid-19-tracker/static/js/main.07380d5b.chunk.js"
+  },
+  {
+    "revision": "f5a2e4780e5e49bf93c5",
+    "url": "/covid-19-tracker/static/js/runtime-main.92976cc4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/covid-19.d7265326.png"
+  }
+]);
