2b582a07ade8b61f8f422c35c48d23b5c33735dc
Mayur <mayur.jaganmohan@gmail.com>
2020-04-30 17:52:31 +0530

    Updated App component and Index.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,38 @@ import React from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api/index";
+import coronaImage from "./images/covid-19.png";
 
 class App extends React.Component {
   state = {
     data: {},
+    country: "",
   };
 
   async componentDidMount() {
     const fetchedData = await fetchData();
+
+    console.log("Fetched data in componentDidMount : " + fetchedData);
+
     this.setState({ data: fetchedData });
   }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
+
+    console.log("In render : " + data);
 
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="Covid-19" />
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
--- src/api/index.js
+++ src/api/index.js
@@ -2,18 +2,51 @@ import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+
   try {
     const {
-      data: { confirmed, recovered, deaths, lastUpdated },
-    } = await axios.get(url);
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(changeableUrl);
 
     return {
       confirmed,
       recovered,
       deaths,
-      lastUpdated,
+      lastUpdate,
     };
+  } catch (error) {
+    console.log("Error caught in fetchData : " + error);
+  }
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    const modifieddata = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modifieddata;
+  } catch (error) {
+    console.log("Error caught in fetchDailyData : " + error);
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
   } catch (error) {
     //
   }
